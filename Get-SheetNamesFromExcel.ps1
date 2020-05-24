b2c550b750e3f9af81522bda1db706c20fdb6638
k-nishizono <kzk.nishizono@gmail.com>
2020-05-24 20:39:38 +0900

    Add the feature getting the cells you want

--- Get-SheetNamesFromExcel.ps1
+++ Get-SheetNamesFromExcel.ps1
@@ -20,6 +20,26 @@
 #            The FindLastCellIgnoringFormatted finds the last cell with value in
 #            contrast with that the FindLastCell finds the last cell formatted or
 #            with value.
+#     -GetCellByPosition
+#            Use the parameter to get values of the cells you know its location.
+#            You specify the names and positions of the cells like below.
+#            > -GetCellByPosition @{ScreenTitle="B1";Author="J1"}
+#            The parameter adds properties whose name is the specified name and
+#            whose value is the value of the cell at the specified position.
+#     -GetCellByLeftTitle
+#            Use the parameter to get values of the cells you know the label
+#            in its left. Specify the names and labels like below.
+#            > -GetCellByLeftTitle @{ScreenTitle="SCREEN NAME";Author="BY"}
+#            The parameter adds properties whose name is the specified name and
+#            whose value is the value of the cell reached by pressing keys
+#            [End]+[Right] at the cell with the title.
+#     -GetCellByTopTitle
+#            Use the parameter to get values of the cells you know the label
+#            in its above. Specify the names and labels like below.
+#            > -GetCellByTopTitle @{ScreenTitle="SCREEN NAME";Author="BY"}
+#            The parameter adds properties whose name is the specified name and
+#            whose value is the value of the cell reached by pressing keys
+#            [End]+[Down] at the cell with the title.
 #   <OUTPUT>
 #            PsCustomObject object that has two properties below.
 #             - "Sheet" : the name of a sheet in the Excel file
@@ -40,7 +60,13 @@ function global:Get-SheetNamesFromExcel {
         [switch]
         $FindLastCell,
         [switch]
-        $FindLastCellIgnoringFormatted
+        $FindLastCellIgnoringFormatted,
+        [Hashtable]
+        $GetCellByPosition,
+        [Hashtable]
+        $GetCellByLeftTitle,
+        [Hashtable]
+        $GetCellByTopTitle
     )
     begin{
         Set-Variable -Name "excel"
@@ -142,12 +168,50 @@ function global:Get-SheetNamesFromExcel {
                     Add-Member -InputObject $tmpObject -MemberType NoteProperty -Name "LastRow" -Value $lastY
                     Add-Member -InputObject $tmpObject -MemberType NoteProperty -Name "LastColumn" -Value $lastX
                 }
+                # Get the values of the cells in the specified positions
+                if ($null -ne $GetCellByPosition) {
+                    foreach ($key in $GetCellByPosition.Keys) {
+                        $value = $_.Range($GetCellByPosition[$key]).Value2
+                        Add-Member -InputObject $tmpObject -MemberType NoteProperty -Name $key -Value $value
+                        Write-Debug "        GetCellByPosition: KEY=$key VALUE=$($GetCellByPosition[$key])) CELL=$($value)"
+                        Clear-Variable -Name "value"
+                    }
+                }
+                # Get the values of the cells which come with the specified title in its left
+                if ($null -ne $GetCellByLeftTitle) {
+                    foreach ($key in $GetCellByLeftTitle.Keys) {
+                        $title = $_.Cells.Find($GetCellByLeftTitle[$key])
+                        Set-Variable -Name "value"
+                        if ($null -ne $title) {
+                            # can not find the type [Microsoft.Office.Interop.Excel.XlDirection]::XlToRight
+                            $value = $title.End(-4161).Value2
+                            Write-Debug "        GetCellByLeftTitle: KEY=$key VALUE=$($GetCellByLeftTitle[$key])) CELL=$($value)"
+                        }
+                        Add-Member -InputObject $tmpObject -MemberType NoteProperty -Name $key -Value $value
+                        Clear-Variable -Name "value"
+                    }
+                }
+                # Get the values of the cells which come with the specified title in its above
+                if ($null -ne $GetCellByTopTitle) {
+                    foreach ($key in $GetCellByTopTitle.Keys) {
+                        $title = $_.Cells.Find($GetCellByTopTitle[$key])
+                        Set-Variable -Name "value"
+                        if ($null -ne $title) {
+                            # can not find the type [Microsoft.Office.Interop.Excel.XlDirection]::XlDown
+                            $value = $title.End(-4121).Value2
+                            Write-Debug "        GetCellByTopTitle: KEY=$key VALUE=$($GetCellByTopTitle[$key])) CELL=$($value)"
+                        }
+                        Add-Member -InputObject $tmpObject -MemberType NoteProperty -Name $key -Value $value
+                        Clear-Variable -Name "value"
+                    }
+                }
                 return $tmpObject
             }
         } catch {
             Write-Error "An error happened while reading the file"
             Write-Error $_.ErrorDetails
             Write-Error $_.ScriptStackTrace
+            Write-Error $_.InvocationInfo
             return
         } finally {
             if($book) {
@@ -168,7 +232,7 @@ function global:Get-SheetNamesFromExcel {
 
 # INSTALLATION
 # (i) to define the function Get-SheetNamesFromExcel to call this file
-#     > ./Get-SheetNamesFromExcel.ps1
+#     > Import-Module Get-SheetNamesFromExcel.ps1 -Force
 # USAGE EXAMPLE
 # (a) to print the sheet names of the Excel files on the current location
 #     > ls *.xlsx | Get-SheetNamesFromExcel | %{$_.Sheet}
